4cf4fdf6a1f649a99e7768a49a6043c0608bd309
iknowaditya <97401096+iknowaditya@users.noreply.github.com>
2024-07-20 16:51:09 +0530

    Update vite.config.js

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,16 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { config } from "dotenv";
+// import { config } from "dotenv";
 
-config(); // Load environment variables
+// config(); // Load environment variables
 
-console.log("VITE_API_URL:", process.env.VITE_API_URL); // Check the value
+// console.log("VITE_API_URL:", process.env.VITE_API_URL); // Check the value
 
 export default defineConfig({
   server: {
-    proxy: {
-      "/api": process.env.VITE_API_URL,
-    },
+    // proxy: {
+    //   "/api": process.env.VITE_API_URL,
+    // },
   },
   plugins: [react()],
 });
