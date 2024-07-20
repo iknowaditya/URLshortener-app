a4a696d0fcfa7dd23fc770e4ebeb923cd57ae07b
iknowaditya <97401096+iknowaditya@users.noreply.github.com>
2024-07-20 16:48:00 +0530

    Update UrlsForm.jsx

--- Client/src/components/UrlsForm.jsx
+++ Client/src/components/UrlsForm.jsx
@@ -5,7 +5,7 @@ import { Input } from "@headlessui/react";
 import { toast } from "react-hot-toast";
 import { FaLink, FaArrowRight } from "react-icons/fa";
 
-// const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL;
 
 const UrlsForm = ({ fetchUrls }) => {
   const [fullUrl, setFullUrl] = useState("");
@@ -17,7 +17,7 @@ const UrlsForm = ({ fetchUrls }) => {
       return;
     }
     try {
-      const res = await axios.post(`https://urlshort-server.onrender.com/api/url/shortUrl`, {
+      const res = await axios.post(`${API_URL}/api/url/shortUrl`, {
         fullUrl: fullUrl,
       });
       console.log("Response from server:", res.data);
@@ -38,7 +38,7 @@ const UrlsForm = ({ fetchUrls }) => {
 
   const getShortUrl = async () => {
     try {
-      const res = await axios.get(`https://urlshort-server.onrender.com/api/url/shortUrl/${shortId}`);
+      const res = await axios.get(`${API_URL}/api/url/shortUrl/${shortId}`);
       console.log(res.data);
     } catch (err) {
       console.log("Error getting short URL:", err);
